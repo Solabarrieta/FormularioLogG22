32b56e7098b386f8edd28aea13df542912869780
Oier <oieruni22@gmail.com>
2021-09-21 00:11:40 +0200

    Expresiones regulares funcionan, falta mejorar css y partes opcionales

--- php/QuestionForm.php
+++ php/QuestionForm.php
@@ -8,34 +8,56 @@
   <section class="main" id="s1">
     <div>
     <form action="">
+      <div>
       <label for="correo">Introduce un correo de UPV/EHU: </label>
       <input type ="text" id="correo" name="correo">  
+      </div>
       
+      <div>
       <label for="enunciado">Enunciado de la pregunta: </label>
       <input type="text" name="enunciado">
+      </div>
 
+      <div>
       <label for="correcta">Respuesta correcta: </label>
       <input type="text" name="correcta">
+      </div>
+
+      <div>
       <label for="incorrecta">Respuesta incorrecta: </label>
       <input type="text" name="incorrecta">
+      </div>
+      
+      <div>
       <label for="incorrecta1">Respuesta incorrecta: </label>
       <input type="text" name="incorrecta1">
+      </div>
+      
+      <div>
       <label for="incorrecta2">Respuesta incorrecta: </label>
       <input type="text" name="incorrecta2">
-      
+      </div>
+
+      <div>
       <label for="complejidad">Complejidad</label>
       <select name="complejidad" id="complejidad">
         <option value="1">Baja</option>
         <option value="2">Media</option>
         <option value="3">Alta</option>
       </select>
-
+      </div>
+      
+      <div>
       <label for="tema">Tema(Subject)*:</label>
       <input type="text" name="tema" id="tema">
+      </div>
 
-      <input type="file" accept="Imagenes/*">
+
+      <div>
+        <input accept="image/png,image/jpeg" type="file">
+      </div>
       
-      <button onclick="testEmail()">Crear pregunta</button>
+      <button class="boton" onclick="testEmail()">Crear pregunta</button>
     </form>
 
     </div>
