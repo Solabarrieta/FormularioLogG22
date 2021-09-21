2624a16519c2568aa4553dff11aba4bd71f4ffa9
Oier <oieruni22@gmail.com>
2021-09-21 19:33:18 +0200

    Haciendo algunos arreglos finales al codigo

--- js/ValidateFieldsQuestionJS.js
+++ js/ValidateFieldsQuestionJS.js
@@ -1,3 +1,38 @@
+function comprobarCamposVacios(){
+
+    let email = document.getElementById("email").value;
+    let enunciado = document.getElementById("enunciado").value;
+    let resCorrecta = document.getElementById("correcta").value;
+    let resIncorrecta = document.getElementById("incorrecta").value;
+    let resIncorrecta1 = document.getElementById("incorrecta1").value;
+    let resIncorrecta2 = document.getElementById("incorrecta2").value;
+    let select = document.getElementById("complejidad").value;
+    let tema = document.getElementById("tema").value;
+
+
+    let requires = [{variable: email, parrafo: "textCorreo"},
+                    {variable: enunciado, parrafo: "textEnunciado"},
+                    {variable: resCorrecta, parrafo: "textCorrecta"},
+                    {variable: resIncorrecta, parrafo: "textIncorrecta"},
+                    {variable: resIncorrecta1, parrafo: "textIncorrecta1"},
+                    {variable: resIncorrecta2, parrafo: "textIncorrecta2"},
+                    {variable: select, parrafo: "textComplejidad"},
+                    {variable: tema, parrafo: "textTema"}]
+    
+    let camposVacios=0; 
+    requires.forEach((entrada)=>{
+        if(entrada.variable === ""){
+            debugger
+            let result = document.getElementById(entrada.parrafo);
+            result.innerText = 'Este campo es obligatorio'
+            camposVacios++;
+        }
+    });
+
+    if(camposVacios===0){
+        testEmail();
+    }
+}
 
 
 
@@ -18,45 +53,11 @@ function testEmail(){
 
 
 
-function comprobarCamposVacios(){
-
-    let email = document.getElementById("email").value;
-    let enunciado = document.getElementById("enunciado").value;
-    let resCorrecta = document.getElementById("correcta").value;
-    let resIncorrecta = document.getElementById("incorrecta").value;
-    let resIncorrecta1 = document.getElementById("incorrecta1").value;
-    let resIncorrecta2 = document.getElementById("incorrecta2").value;
-    let select = document.getElementById("complejidad").value;
-    let tema = document.getElementById("tema").value;
-    let image = document.getElementById("inputFile").value;
-
-
-    let requires = [{variable: email, parrafo: "textCorreo"},
-                    {variable: enunciado, parrafo: "textEnunciado"},
-                    {variable: resCorrecta, parrafo: "textCorrecta"},
-                    {variable: resIncorrecta, parrafo: "textIncorrecta"},
-                    {variable: resIncorrecta1, parrafo: "textIncorrecta1"},
-                    {variable: resIncorrecta2, parrafo: "textIncorrecta2"},
-                    {variable: select, parrafo: "textComplejidad"},
-                    {variable: tema, parrafo: "textTema"},
-                    {variable: image, parrafo: "textFile"}]
-    
-    let camposVacios=0; 
-    requires.forEach((entrada)=>{
-        if(entrada.variable === ""){
-            debugger
-            let result = document.getElementById(entrada.parrafo);
-            result.innerText = 'Este campo es obligatorio'
-            camposVacios++;
-        }
-    });
-    if(camposVacios===0){
-        testEmail();
-    }
-}
 
 function enviarCuestionario(){
     alert("Pregunta Creada correctamente!!");
 
 }
+
+
 
