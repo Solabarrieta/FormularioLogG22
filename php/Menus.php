ef8ff1d4c97a6f2f8dc66b06c1b46533091e46a8
Oier <oieruni22@gmail.com>
2021-09-23 18:35:59 +0200

    He intentado cambiar los estilos de los hipervinculos, pero no he podido sobreescribir los estilos del navegador...

--- php/Menus.php
+++ php/Menus.php
@@ -6,8 +6,8 @@
 
 </header>
 <nav class='main' id='n1' role='navigation'>
-  <span><a href='Layout.php'>Inicio</a></span>
-  <span><a href='AddQuestionWithImage.php'> Insertar Pregunta</a></span>
-  <span><a href='Credits.php'>Creditos</a></span>
+  <span><a class="ancla" href='Layout.php'>Inicio</a></span>
+  <span><a class= "ancla" href='AddQuestionWithImage.php'> Insertar Pregunta</a></span>
+  <span><a class= "ancla" href='Credits.php'>Creditos</a></span>
 </nav>
 
