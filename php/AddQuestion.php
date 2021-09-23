8e034aa294e6656169615a89f2f98b3953e58d7c
Oier <oieruni22@gmail.com>
2021-09-23 18:18:36 +0200

    Este es el codigo HTML donde he linkeado el script de JQuery

--- php/AddQuestion.php
+++ php/AddQuestion.php
@@ -2,13 +2,73 @@
 <html>
 <head>
   <?php include '../html/Head.html'?>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="../js/ValidateFieldsQuestionJQ.js"></script>
 </head>
 <body>
   <?php include '../php/Menus.php' ?>
   <section class="main" id="s1">
     <div>
 
-			Código PHP para añadir una pregunta sin imagen
+    <div>
+      <label for="correo">Introduce un correo de UPV/EHU: </label>
+      <input type ="text" id="email" name="correo">  
+      <p id="textEmail"></p>
+      </div>
+      
+      <div>
+      <label for="enunciado">Enunciado de la pregunta: </label>
+      <input type="text" name="enunciado" id="enunciado">
+      <p id="textEnunciado"></p>
+      </div>
+
+      <div>
+      <label for="correcta">Respuesta correcta: </label>
+      <input type="text" name="correcta" id="correcta">
+      <p id="textCorrecta"></p>
+      </div>
+
+      <div>
+      <label for="incorrecta">Respuesta incorrecta: </label>
+      <input type="text" name="incorrecta" id="incorrecta">
+      <p id="textIncorrecta"></p>
+      </div>
+      
+      <div>
+      <label for="incorrecta1">Respuesta incorrecta: </label>
+      <input type="text" name="incorrecta1" id="incorrecta1">
+      <p id="textIncorrecta1"></p>
+      </div>
+      
+      <div>
+      <label for="incorrecta2">Respuesta incorrecta: </label>
+      <input type="text" name="incorrecta2"id="incorrecta2">
+      <p id="textIncorrecta2"></p>
+      </div>
+
+      <div>
+      <label for="complejidad">Complejidad</label>
+      <select name="complejidad" id="complejidad">
+        <option value="1">Baja</option>
+        <option value="2">Media</option>
+        <option value="3">Alta</option>
+      </select>
+      <p id="textComplejidad"></p>
+      </div>
+      
+      <div>
+      <label for="tema">Tema(Subject)*:</label>
+      <input type="text" name="tema" id="tema">
+      <p id="textTema"></p>
+      </div>
+
+
+      <div>
+        <input accept="image/png,image/jpeg" type="file" id=inputFile>
+      </div>
+
+      <button type="button" id="boton">Crear pregunta</button>
+    </form>
 
     </div>
   </section>
