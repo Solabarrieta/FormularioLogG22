46728ea71a8d7364dc5628fb796b7ce48b40c9a4
Oier <oieruni22@gmail.com>
2021-10-21 11:29:56 +0200

    Se ha añadido un enlace para poder visualizar el contenido del .json

--- php/Menus.php
+++ php/Menus.php
@@ -15,7 +15,8 @@ $variables = "?user=$user"
     <nav class='main' id='n1' role='navigation'>
       <span><a class="ancla" href='Layout.php<?php echo "$variables" ?>'>Inicio</a></span>
       <span><a class="ancla" href='QuestionForm.php<?php echo "$variables" ?>'> Insertar Pregunta</a></span>
-      <span><a class="ancla" href='ShowQuestions.php<?php echo "$variables" ?>'> Insertar Pregunta</a></span>
+      <span><a class="ancla" href='ShowQuestions.php<?php echo "$variables" ?>'> Ver preguntas</a></span>
+      <span><a class="ancla" href='ShowQuestionsJson.php<?php echo "$variables" ?>'> Ver preguntas JSON</a></span>
       <span><a class="ancla" href='Credits.php<?php echo "$variables" ?>'>Creditos</a></span>
     </nav>
   </form>
