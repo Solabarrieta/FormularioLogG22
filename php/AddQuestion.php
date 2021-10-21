a504a946fd4e29a04026056f57a016884e1426aa
Oier <oieruni22@gmail.com>
2021-10-21 13:32:34 +0200

    Intentando arreglar el añadir pregunta

--- php/AddQuestion.php
+++ php/AddQuestion.php
@@ -17,7 +17,7 @@
           printf("Error de conexión: %s\n", mysqli_connect_error());
           exit();
         }
-          
+
 
 
         $stmt = $conn->prepare("INSERT INTO preguntas (email, enunciado,respuestaCorrecta, respuestaIncorrecta1, respuestaIncorrecta2, respuestaIncorrecta3, complejidad, tema) VALUES ('" . $_GET['user'] . "','" . $_GET['enunciado'] . "','" . $_GET["correcta"] . "','" . $_GET["incorrecta"] . "','" . $_GET["incorrecta1"] . "','" . $_GET["incorrecta2"] . "','" . $_GET["complejidad"] . "','" . $_GET["tema"] . "')");
@@ -25,13 +25,14 @@
         $user = $_GET['user'];
         $actual_link = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
-        
+
 
         if ($stmt) {
           if ($stmt->execute()) {
+            echo "hola";
             echo "se ha enviado la pregunta correctamente";
-            $stmt->close();
             echo "<p><a href='./ShowQuestions.php?user=$user'>Ver preguntas</a></p>";
+            $stmt->close();
           }
         } else {
           echo 'no se ha podido enviar la pregunta a la base de datos';
