0662964e7a322808c576aa19d4eb438ec6a3e8dd
Oier <oieruni22@gmail.com>
2021-10-24 14:02:52 +0200

    Quitando elementos del encabezado de la tabal que no se requerian segun el enunciado

--- php/ShowQuestionsJson.php
+++ php/ShowQuestionsJson.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+!DOCTYPE html>
 
 <html lang="en">
 
@@ -21,22 +21,18 @@
         $array = json_decode($data);
         echo '<h3><center>CONTENIDO DEL FICHERO JSON </h3><p><p>';
         echo '<body>';
-        echo '<p><center><table ><tr><th>Tema</th><th>Autor</th><th>Enunciado</th><th>Respuesta Correcta</th><th>Respuesta incorrecta 1</th><th>Respuesta Incorrecta 2</th><th>Respuesta Incorrecta 3</th></tr>';
+        echo '<p><center><table ><tr><th>Autor</th><th>Enunciado</th><th>Respuesta Correcta</th></tr>';
         foreach ($array->assessmentItems as $question) {
-            $confirmUser = $question->author;
-            if ($confirmUser == $user) {
-                $author =  $question->author;
+            $author = $question->author;
+            if ($author == $user) {
                 $itemBody = $question->itemBody->p;
                 $correctResponse = $question->correctResponse->value;
-                /* $incorrectResponses = $question->incorrectResponses->value[0];
-            $incorrectResponse1 = $question->incorrectResponses->value[1];
-            $incorrectResponse2 = $question->incorrectResponses->value[2];*/
                 echo '<tr>
 
-    <td>' . $author . '</td>
-    <td>' . $itemBody . '</td>
-    <td>' . $correctResponse . '</td>
-    </tr>';
+                <td>' . $author . '</td>
+                <td>' . $itemBody . '</td>
+                <td>' . $correctResponse . '</td>
+                </tr>';
             }
         }
         echo '</table><center>';
